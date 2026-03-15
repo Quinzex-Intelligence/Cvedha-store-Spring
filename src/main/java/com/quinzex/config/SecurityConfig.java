7137e6ccd007ff63d0929aa0619f95a364c07b43
akhil <malisettybhargavakhil25@gmail.com>
2026-03-15 20:27:02 +0530

    change of cors change 3

--- src/main/java/com/quinzex/config/SecurityConfig.java
+++ src/main/java/com/quinzex/config/SecurityConfig.java
@@ -35,14 +35,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOriginPatterns(
-                List.of(
-                        "http://localhost:3000",
-                        "https://quinzexintelligence.com",
-                        "https://www.quinzexintelligence.com",
-                        "https://*.quinzexintelligence.com"
-                )
-        );
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000","https://www.quinzexintelligence.com","https://www.d2sg5wp92ge742.cloudfront.net","https://www.quinzexintelligence.com","http://api.quinzexintelligence.com","https://backend.quinzexintelligence.com","https://api.quinzexintelligence.com"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         corsConfiguration.setAllowCredentials(true);
